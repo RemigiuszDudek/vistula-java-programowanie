1e8a6ffc622329a0442d9241e252ddd289257acf
remis@op.pl <remis@op.pl>
2016-11-09 04:24:51 +0100

    add meaningful geometry test

--- src/test/java/org/vistula/dudekre/unittest/GeometryTest.java
+++ src/test/java/org/vistula/dudekre/unittest/GeometryTest.java
@@ -2,26 +2,21 @@ package org.vistula.dudekre.unittest;
 
 import org.junit.Test;
 import org.vistula.dudekre.geometry.Point;
-import org.vistula.dudekre.other.Puppy;
-import org.vistula.dudekre.other.WashingMashine;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GeometryTest {
     @Test
     public void cos() {
+        //given
         Point p1 = new Point(1, 2);
         Point p2 = new Point(3, 4);
+        Point expectedPointSum = new Point(4, 6);
+
+        //when
         Point p3 = p1.add(p2);
+
+        //then
+        assertThat(p3).isEqualToComparingFieldByField(expectedPointSum);
     }
-
-    @Test
-    public void wm() {
-        Puppy puppy = new Puppy("Azor");
-    }
-
-
-
-
-
-
-
 }

eba3844302ec0858033e06a198682d4e264db757
Remigiusz Dudek <remigiusz_dudek@tlen.pl>
2016-11-29 05:46:21 +0100

    next portion of homeworks

--- src/test/java/org/vistula/homework/day1/sss/corrected/Rectangle.java
+++ src/test/java/org/vistula/homework/day1/sss/corrected/Rectangle.java
@@ -3,23 +3,20 @@ package org.vistula.homework.day1.sss.corrected;
 /**
  * Created by Adams on 2016-11-25.
  */
-public class Rectangle{
-    //szerokość
-    int x;
-
-    //wysokość
-    int y;
+public class Rectangle {
+    private int x;
+    private int y;
 
     public Rectangle(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public int calculateField(){
-        return x*y;
+    public int calculateField() {
+        return x * y;
     }
 
-    public int calculatePerimeter(){
-        return x*2 + y*2;
+    public int calculatePerimeter() {
+        return x * 2 + y * 2;
     }
 }
